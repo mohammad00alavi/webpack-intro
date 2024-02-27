507b9ee11a76b2b4422b51a8e306f820a059eced
Mohammad Alavi <43592603+mohammad00alavi@users.noreply.github.com>
2024-02-27 17:26:17 +0000

    added webpack configs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+import jokeGenerator from "./jokeGenerator";
+import "./styles/main.scss";
+import laugh from "./assets/laugh.svg";
+
+const laughImg = document.getElementById("laughImg");
+laughImg.src = laugh;
+
+const jokeBtn = document.getElementById("jokeBtn");
+jokeBtn.addEventListener("click", jokeGenerator);
+
+jokeGenerator();
